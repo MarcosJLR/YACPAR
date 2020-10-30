f0cbe52d8c515c67ebec5c8153418dc3fc810503
MarcosJLR <socram.693@gmail.com>
2020-10-30 03:48:26 -0400

    Added time measurement

--- tests/C++/Strings/AhoCorasick.cpp
+++ tests/C++/Strings/AhoCorasick.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using namespace std::chrono;
 
 #include "../../../src/C++/Strings/AhoCorasick.hpp"
 
@@ -37,7 +38,7 @@ bool test(vector<string>& pats, string& text)
     sort(result.begin(), result.end());
     sort(naive.begin(), naive.end());
 
-    //if(result.size() != naive.size()) return false;
+    if(result.size() != naive.size()) return false;
     for(int i = 0; i < (int) result.size(); i++){
         if(result[i] != naive[i])
             return false;
@@ -50,7 +51,7 @@ int main(){
     
     srand(time(NULL));
 
-    vector <int> sizes = { 10, 100, 1000 };
+    vector <int> sizes = { 10, 100, 1000, 300000 };
 
     for(int k = 0; k < 10; k++){
         for(int sz : sizes){
@@ -59,7 +60,7 @@ int main(){
             set <string> uPats;
             string text = "";
             while(acum < sz){
-                string s = randString(rand() % sz + 1);
+                string s = randString(rand() % 10 + 1);
                 if(uPats.count(s)) continue;
                 pats.push_back(s);
                 uPats.insert(s);
@@ -67,11 +68,53 @@ int main(){
                 if(rand() % 2) text += pats.back();
             }
 
-            assert(test(pats, text));
+            if(sz <= 1000) assert(test(pats, text));
+            else{
+                auto start = high_resolution_clock::now();
+
+                AhoCorasick ac;
+                for(string& p : pats) ac.add(p); 
+                auto result = ac.feed(text);
+
+                auto stop = high_resolution_clock::now();
+                auto duration = duration_cast<milliseconds>(stop-start);
+                
+                cout << "Test with sum of lengths: " << acum << endl;
+                cout << "Number of Matches: " << result.size() << endl;
+                cout << "Processed in " << duration.count();
+                cout << " milliseconds" << endl << endl;
+
+            }
         }
     }
+
+    int acum = 0;
+    vector <string> pats;
+    string text = "";
+    for(int i = 1; acum < sizes.back(); i++){
+        string s = "";
+        for(int j = 1; j <= i; j++) s += 'a';
+        pats.push_back(s);
+        acum += i;
+    }
+    text = pats.back();
+
+    auto start = high_resolution_clock::now();
+
+    AhoCorasick ac;
+    for(string& p : pats) ac.add(p); 
+    auto result = ac.feed(text);
+
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop-start);
     
-    printf("Test passed!\n");
+    cout << "Corner case test with sum of lengths: " << acum << endl;
+    cout << "Number of Matches: " << result.size() << endl;
+    cout << "Processed in " << duration.count();
+    cout << " milliseconds" << endl << endl;
+
+    
+    cout << "Test passed!" << endl;
 
 
     return 0;
